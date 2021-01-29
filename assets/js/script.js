63be53bc4ded094560716a817d54ebd4b465a1f2
byrnelorcan <lorcan.byrne@icloud.com>
2021-01-29 22:40:18 +0000

    add js for scrollspy and smooth scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,44 @@ mymap = L.map('mapid').setView([53.5267, -6.5488], 13);
 
                             var marker = L.marker([53.5267613,-6.5488573]).addTo(mymap);
 
+
+// https://stackoverflow.com/questions/42401606/how-to-hide-collapsible-bootstrap-4-navbar-on-click
 $('.navbar-nav>li>a').on('click', function(){
     $('.navbar-collapse').collapse('hide');
 });
+
+
+//https://gist.github.com/ambitstream/e69f9937fc0f2e22688425f8ecfe5a19
+ 
+//scrollSpy function
+    function scrollSpy() {
+      var sections = ['head','services', 'technology', 'about', 'contact'];
+      var current;
+
+      for (var i = 0; i < sections.length; i++) {
+        if ( $('#'+sections[i]).offset().top <= $(window).scrollTop() ) {
+          current = sections[i];
+        }
+      }
+
+      $("nav a[href='#"+current+"']").addClass('active');
+      $("nav a").not("a[href='#"+current+"']").removeClass('active');      
+    }
+
+// smooth scrolling navigation
+    $("nav li a").click( function() {
+      var target = $(this).attr("href");
+      $("body, html").animate({
+        scrollTop: $(target).offset().top
+      }, 300);
+      return false;
+    });
+
+    //scrollSpy call
+    $(document).ready( function() {
+      scrollSpy();
+    });
+
+    $(window).scroll( function() {
+      scrollSpy();
+    });
